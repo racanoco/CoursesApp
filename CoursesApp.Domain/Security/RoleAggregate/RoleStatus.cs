a88c831e638a96cd768e5b1ef91b778108f18ca2
Rcano <Rcanocorbera@gmail.com>
2022-09-02 16:30:13 +0200

    Create status enumeration

---
+++ CoursesApp.Domain/Security/RoleAggregate/RoleStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    public enum RoleStatus
+    {
+        Active = 1,
+        Inactive = 2,
+    }
+}   
---
+++ CoursesApp.Domain/Security/RoleAggregate/UserStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    public enum UserStatus
+    {
+        Active = 1,
+        Inactive = 2,
+    }
+}
