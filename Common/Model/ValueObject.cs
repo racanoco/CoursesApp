a28f5999d7725ec70cdf9e00b9752083c7da5d4f
Rcano <Rcanocorbera@gmail.com>
2022-09-05 18:15:20 +0200

    patter ValueObject

---
+++ Common/Model/ValueObject.cs
@@ -0,0 +1,69 @@
+﻿namespace Common.Model
+{
+    /// <summary>
+    /// Patrón ValueObject
+    /// </summary>
+    public abstract class ValueObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public abstract IEnumerable<object> GetAtomicValues();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valueObjectOne"></param>
+        /// <param name="valueObjectTwo"></param>
+        /// <returns></returns>
+        public static bool operator ==(ValueObject valueObjectOne, ValueObject valueObjectTwo)
+        {
+            return valueObjectOne?.Equals(valueObjectTwo) ?? false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valueObjectOne"></param>
+        /// <param name="valueObjectTwo"></param>
+        /// <returns></returns>
+        public static bool operator !=(ValueObject valueObjectOne, ValueObject valueObjectTwo)
+        {
+            return !(valueObjectOne?.Equals(valueObjectTwo) ?? false);
+        }       
+
+        /// <summary>
+        /// Sobrescribimos el método Equals para un comportamiento personalizado.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            ValueObject? other = obj as ValueObject;
+            IEnumerator<object> thisValues = GetAtomicValues().GetEnumerator();
+            IEnumerator<object> otherValues = other.GetAtomicValues().GetEnumerator();
+
+            while (thisValues.MoveNext() && otherValues.MoveNext())
+            {
+                if(ReferenceEquals(thisValues.Current, null) != ReferenceEquals(otherValues.Current, null))
+                {
+                    return false;
+                }
+
+                if(thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
+                {
+                    return false;
+                }
+            }
+
+            return !thisValues.MoveNext() && !otherValues.MoveNext();
+            
+        }
+    }
+}
---
+++ CoursesApp.Domain/Security/RoleAggregate/Address.cs
@@ -0,0 +1,32 @@
+﻿using Common.Model;
+
+namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    public class Address: ValueObject
+    {
+        #region PROPERTIES
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }  
+        public string ZipCode { get; private set; }
+        #endregion
+
+        #region BUILDER
+        public Address(string street, string city, string state, string country, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+        }
+
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return new object[] { Street, City, State, Country, ZipCode };
+        }
+
+        #endregion
+    }
+}
