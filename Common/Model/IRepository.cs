623a2fbaf6d66b0dcc331ebed642efca15e8b658
Rcano <Rcanocorbera@gmail.com>
2022-09-06 10:57:13 +0200

    Create interface to the repository aggregate root

---
+++ Common/Model/IRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Linq.Expressions;
+
+namespace Common.Model
+{
+    public interface IRepository<T> where T : AggregateRoot
+    {
+        T GetById(Guid id);
+        T Find(Expression<Func<T, bool>> match);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> match);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(Guid id);
+    }
+}
---
+++ CoursesApp.Domain/Security/RoleAggregate/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Common.Model;
+
+namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    public interface IRoleRepository: IRepository<Role>
+    {
+        List<Role> GetByStatus(RoleStatus roleStatus);
+        void AddUser(User user);
+        Role GetByUserId(Guid userId);
+        Role GetByUserCode(string userCode);
+    }
+}
