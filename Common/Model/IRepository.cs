9066b6db2132dd52660f9aa7c2179b7d4a84d7d5
Rcano <Rcanocorbera@gmail.com>
2022-09-08 09:47:11 +0200

    Create interface for the repository Aggregate Root

--- Common/Model/IRepository.cs
+++ Common/Model/IRepository.cs
@@ -1,15 +1,55 @@
-﻿
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
 namespace Common.Model
 {
+    /// <summary>
+    /// Generic repository interface that can be used by any type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IRepository<T> where T : AggregateRoot
     {
+        /// <summary>
+        /// Methods of persistence and access to the database.
+        /// </summary>        
+
+
+        #region GET
         T GetById(Guid id);
+
+        /// <summary>
+        /// Get record by any field
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns></returns>
         T Find(Expression<Func<T, bool>> match);
+
+        /// <summary>
+        /// Get multiple records by any field
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns></returns>
         IEnumerable<T> FindAll(Expression<Func<T, bool>> match);
+        #endregion
+
+        #region POST
         void Add(T entity);
+        #endregion
+
+        #region PUT
         void Update(T entity);
+        #endregion
+
+        #region DELETE
         void Delete(Guid id);
+        #endregion
+
+
+
+
+
+
+
+
+
     }
 }
--- CoursesApp.Domain/Security/RoleAggregate/IRoleRepository.cs
+++ CoursesApp.Domain/Security/RoleAggregate/IRoleRepository.cs
@@ -4,9 +4,27 @@ namespace CoursesApp.Domain.Security.RoleAggregate
 {
     public interface IRoleRepository: IRepository<Role>
     {
+
+        #region GET
         List<Role> GetByStatus(RoleStatus roleStatus);
-        void AddUser(User user);
         Role GetByUserId(Guid userId);
         Role GetByUserCode(string userCode);
+        #endregion
+
+        #region POST
+        void AddUser(User user);
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region DELETE
+
+        #endregion
+
+
+
+        
     }
 }
