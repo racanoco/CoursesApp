e70eb7212f947f3de75ba126a5223aa25b269dbe
Rcano <Rcanocorbera@gmail.com>
2022-09-09 12:09:12 +0200

    Create interface to define a UnitOfWork

---
+++ CoursesApp.Domain/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Common.Model;
+using CoursesApp.Domain.Security.RoleAggregate;
+
+namespace CoursesApp.Domain
+{
+    public interface IUnitOfWork
+    {
+        IRoleRepository _roleRepository { get; }
+
+        void Commit(AggregateRoot aggregateRoot);
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollBackTransaction();
+        void Dispose();
+    }
+}
