cc715fa97e6ba5815f723d2344714e114be1dad6
Rcano <Rcanocorbera@gmail.com>
2022-09-16 10:56:34 +0200

    Create custom logger class

---
+++ CoursesApp.Infrastructure/CoursesAppLogger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CoursesApp.Infrastructure
+{
+    public class CoursesAppLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{logLevel}: ");
+            Console.ResetColor();
+
+            Console.WriteLine(formatter(state, exception));
+            Console.WriteLine();
+        }
+    }
+
+    public class CoursesAppLoggerProvider : ILoggerProvider
+    {
+        private readonly List<ILogger> loggers = new List<ILogger>();
+        public ILogger CreateLogger(string categoryName)
+        {
+            var logger = new CoursesAppLogger();
+            loggers.Add(logger);
+
+            return logger;
+        }
+
+        public void Dispose() => loggers.Clear();
+        
+    }
+}
