4f0e7705071312140430cb820b9dd675cf9ecd06
Rcano <Rcanocorbera@gmail.com>
2022-09-16 15:54:55 +0200

    Create custom exception class

---
+++ CoursesApp.Infrastructure/Exceptions/RepeatedUniqueIndexException.cs
@@ -0,0 +1,19 @@
+﻿namespace CoursesApp.Infrastructure.Exceptions
+{
+    public class RepeatedUniqueIndexException : Exception
+    {
+        public RepeatedUniqueIndexException()
+        {
+        }
+
+        public RepeatedUniqueIndexException(string message)
+            : base(message)
+        {
+        }
+
+        public RepeatedUniqueIndexException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
