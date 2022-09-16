23588c0a68c4b893efa234277a5e6533eb280af1
Rcano <Rcanocorbera@gmail.com>
2022-09-16 10:32:16 +0200

    add connection database file and add class read connection string.

---
+++ CoursesApp.Infrastructure/ConfigurationManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CoursesApp.Infrastructure
+{
+    public class ConfigurationManager
+    {
+        public static IConfiguration AppSettings { get; }
+        static ConfigurationManager()
+        {
+            AppSettings = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json")
+                .Build();
+        }
+    }
+}
