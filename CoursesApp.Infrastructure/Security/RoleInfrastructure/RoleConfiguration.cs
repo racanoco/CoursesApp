c8b1fc3c102eba5ae275e3ca4b489b95eb04b77c
Rcano <Rcanocorbera@gmail.com>
2022-09-17 12:16:01 +0200

    Create Mapping configuration for the Entity Domain

---
+++ CoursesApp.Infrastructure/Security/RoleInfrastructure/RoleConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using CoursesApp.Domain.Security.RoleAggregate;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace CoursesApp.Infrastructure.Security.RoleInfrastructure
+{
+    internal class RoleConfiguration
+    {
+        internal RoleConfiguration(DbModelBuilder modelBuilder)
+        {
+            // Obtener entidad
+            EntityTypeConfiguration<Role> entity = modelBuilder.Entity<Role>();
+
+            // Mapear tabla y esquema
+            entity.ToTable("Roles", "Security");
+
+            // Crear atributos de tabla
+            entity.HasKey(p => p.Id);
+
+            entity.Property(p => p.Id)
+                .HasColumnName("Id")
+                .HasColumnType("uniqueidentifier")
+                .IsRequired();
+
+            entity.Property(p => p.Code)
+                .HasColumnName("Code")
+                .HasColumnType("varchar")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            entity.Property(p => p.Name)
+                .HasColumnName("Name")
+                .HasColumnType("varchar")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            entity.Property(p => p.Description)
+                .HasColumnName("Description")
+                .HasColumnType("varchar")
+                .HasMaxLength(500)
+                .IsRequired();
+
+            entity.Property(p => p.Status)
+                .HasColumnName("Status")
+                .HasColumnType("int")
+                .IsRequired();
+
+            // Crear relaciones foraneas one-to-many relationships
+            entity.HasMany(p => p.Users);
+
+            // Crear indices
+            entity.HasIndex(p => p.Code).IsUnique();
+
+            entity.HasIndex(p => p.Name).IsUnique();
+
+            entity.HasIndex(p => p.Status);
+        }
+    }
+}
