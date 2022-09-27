3d801f60cc3bb300eba122519f601d3208393e3b
Rcano <Rcanocorbera@gmail.com>
2022-09-27 18:00:25 +0200

    Crear DTOs y Mapping

---
+++ CoursesApp.Application/Security/RoleApplication/DTOs/RoleDTO.cs
@@ -0,0 +1,27 @@
+﻿using CoursesApp.Domain.Security.RoleAggregate;
+
+namespace CoursesApp.Application.Security.RoleApplication.DTOs
+{
+    public class RoleDTO
+    {
+        public Guid Id { get; private set; }
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public RoleStatus Status { get; private set; }
+
+        public static RoleDTO GetRoleDTO(Role role)
+        {
+            return new RoleDTO()
+            {
+                Id = role.Id,
+                Code = role.Code,
+                Name = role.Name,
+                Description = role.Description,
+                Status = role.Status
+            };
+        }
+    }
+
+    
+}
